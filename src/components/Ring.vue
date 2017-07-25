9e4e85dd251b5e064cf984bd4a57f907932768ef
Andrew Rady <andrew.arsoftware@gmail.com>
2017-07-24 23:35:50 -0600

    added basic table

--- src/components/Ring.vue
+++ src/components/Ring.vue
@@ -1,5 +1,29 @@
 <template>
-  
+    <div class="container">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Ring Number</th>
+                        <th>Age</th>
+                        <th>Rank</th>
+                        <th>Division</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>15-17</td>
+                        <td>2nd-3rd Degree</td>
+                        <td>Traditional</td>
+                        <td>Boys</td>
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
